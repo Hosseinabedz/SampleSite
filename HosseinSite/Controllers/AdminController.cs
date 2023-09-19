77a1affb681b5c58cc2f71650a5b8ce777cbfdc4
HosseinAbedZ <hossein.abedz3937@gmail.com>
2023-09-19 12:29:00 +0330

    Add project files.

---
+++ HosseinSite/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HosseinSite.Controllers
+{
+	public class AdminController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+        [HttpPost]
+        public IActionResult Index(string email)
+        {
+            return View();
+        }
+    }
+}
