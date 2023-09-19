25e241351c58a8511fc169133ca7203f1a4dca2c
HosseinAbedZ <hossein.abedz3937@gmail.com>
2023-09-19 12:59:19 +0330

    Initial Experience, Education and MySkills Models

---
+++ HosseinSite/Models/Entities/Experience/Experience.cs
@@ -0,0 +1,12 @@
+﻿namespace HosseinSite.Models.Entities.Experience;
+
+public class Experience
+{
+    public int Id { get; set; }
+    public string EducationTitle { get; set; }
+    public string EducationDuration { get; set; }
+    public string Description { get; set; }
+    public string CompanyName { get; set; }
+    public string? CompanySite { get; set; }
+
+}
